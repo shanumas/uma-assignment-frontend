7d3e15986b0c6c4bd96ea823f6dca36fbfc02acb
Uma Shankar Subramani <uma.shankar@solme.se>
2020-12-09 16:39:10 +0100

    Added other properties to booking

--- booking/src/app/classes/booking.ts
+++ booking/src/app/classes/booking.ts
@@ -1,4 +1,12 @@
 export class Booking{
   id: number = 0;
   name: string = "";
+  contactNumber:string =""
+  pickupTime: string =""
+  waitingTime:String =""
+  passengers:number =1
+  price:string =""
+  rating:string =""
+  createdOn:string =""
+  modifiedOn:string=""
 }
